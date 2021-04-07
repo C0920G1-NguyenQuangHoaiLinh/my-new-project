2c0665a4cc56130e5d109e5d6338585f53bc7e77
C0920G1-NguyenQuangHoaiLinh <68835420+C0920G1-NguyenQuangHoaiLinh@users.noreply.github.com>
2021-04-07 11:51:10 +0700

    test_fcorp

---
+++ New folder/Search.java
@@ -0,0 +1,28 @@
+package test_fcorp;
+
+import java.util.Scanner;
+
+public class Search {
+
+
+    public static void main(String[] args) {
+        String docs_0 = "Bank regulation: Bank regulation is a form of government regulation which subjects banks to certain requirements";
+        String docs_1 = "Banking crisis: When banks suffer a sudden rush of withdrawals by depositors, this is called a bank run";
+        Search.searchString(docs_0, docs_1);
+    }
+
+    public static void searchString(String str1, String str2) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string :");
+        String inputString = scanner.nextLine();
+        if (str1.contains(inputString) && str2.contains(inputString)) {
+            System.out.println("[" + 0 + "," + 1 + "]");
+        } else if (str2.contains(inputString)) {
+            System.out.println("[" + 1 + "]");
+        } else if (str1.contains(inputString)) {
+            System.out.println("[" + 0 + "]");
+        } else {
+            System.out.println(inputString + "not found");
+        }
+    }
+}
